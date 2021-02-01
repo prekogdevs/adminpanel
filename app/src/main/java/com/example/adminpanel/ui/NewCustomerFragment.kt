e3151d8311c6aad4141f5f02f2342ca505cc1720
prekogdevs <gergelyszebenyi@gmail.com>
2021-02-01 18:59:22 +0100

    UI update

--- app/src/main/java/com/example/adminpanel/ui/NewCustomerFragment.kt
+++ app/src/main/java/com/example/adminpanel/ui/NewCustomerFragment.kt
@@ -3,4 +3,4 @@ package com.example.adminpanel.ui
 import androidx.fragment.app.Fragment
 import com.example.adminpanel.R
 
-class CustomerDetailFragment : Fragment(R.layout.fragment_details)
+class NewCustomerFragment : Fragment(R.layout.fragment_new_customer)
