c33cc66d3dc5e5ee41e2642fb065fe77530f9880
prekogdevs <gergelyszebenyi@gmail.com>
2021-02-13 22:32:47 +0100

    Basic logging added to retrofit

--- app/src/main/java/com/example/adminpanel/api/CustomerService.kt
+++ app/src/main/java/com/example/adminpanel/api/CustomerService.kt
@@ -5,6 +5,8 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
@@ -13,20 +15,29 @@ import retrofit2.http.POST
 
 private const val BASE_URL = "http://10.0.2.2:8080/"
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+private val httpLoggingInterceptor = createHTTPLoggingInterceptor()
+private val okHTTPClientBuilder = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor)
 private val retrofit =
     Retrofit
         .Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .client(okHTTPClientBuilder.build())
         .baseUrl(BASE_URL)
         .build()
+
+fun createHTTPLoggingInterceptor(): HttpLoggingInterceptor {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    return httpLoggingInterceptor
+}
 
 interface CustomerService {
     @GET("customer")
     fun getAllcustomers(): Deferred<List<Customer>>
 
     @POST("customer")
-    fun addCustomer(@Body customer : Customer) : Deferred<Void>
+    fun addCustomer(@Body customer: Customer): Deferred<Void>
 }
 
 
